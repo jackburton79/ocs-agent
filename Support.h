6a9ffe43cabb82d3bc3ed1901f8f2cad03ad0516
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-07-11 17:15:15 +0200

    Added std::string::trim

--- Support.h
+++ Support.h
@@ -8,8 +8,10 @@
 #ifndef SUPPORT_H_
 #define SUPPORT_H_
 
+#include <algorithm>
 #include <stdio.h>
 #include <streambuf>
+#include <string>
 
 class popen_streambuf : public std::streambuf {
 public:
@@ -25,5 +27,20 @@ private:
 	FILE* fFile;
 	char_type* fBuffer;
 };
+
+
+static inline std::string& ltrim(std::string& s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
+
+static inline std::string& rtrim(std::string& s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+static inline std::string& trim(std::string& s) {
+	return ltrim(rtrim(s));
+}
 
 #endif /* SUPPORT_H_ */
