c11970bfc900b342efe22f5aecbc806529bbbbaf
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-07-12 10:27:49 +0200

    Print error in case of an exception

--- main.cpp
+++ main.cpp
@@ -7,12 +7,15 @@
 
 #include "Agent.h"
 
+#include <iostream>
+
 int main()
 {
 	try {
 		Agent agent;
 		agent.Run();
 	} catch (...) {
+		std::cerr << "Error." << std::endl;
 		return 1;
 	}
 	return 0;
