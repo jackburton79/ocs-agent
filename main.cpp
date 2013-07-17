e0dadcfd6eb5714a19ec5b62dae00e9cd264a25c
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-07-18 00:20:45 +0200

    Catch specific exceptions

--- main.cpp
+++ main.cpp
@@ -8,14 +8,21 @@
 #include "Agent.h"
 
 #include <iostream>
+#include <string.h>
 
 int main()
 {
 	try {
 		Agent agent;
 		agent.Run();
+	} catch (std::string errorString) {
+		std::cerr << errorString << std::endl;
+		return 1;
+	} catch (int error) {
+		std::cerr << strerror(error) << std::endl;
+		return 1;
 	} catch (...) {
-		std::cerr << "Error." << std::endl;
+		std::cerr << "Unhandled exception." << std::endl;
 		return 1;
 	}
 	return 0;
