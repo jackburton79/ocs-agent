1fd7c99a56a0dd5d97c7658c339c117efd50e37b
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-07-18 00:21:04 +0200

    Also return process memory and swap info

--- RunningProcessesList.cpp
+++ RunningProcessesList.cpp
@@ -7,6 +7,7 @@
 
 #include "ProcReader.h"
 #include "RunningProcessesList.h"
+#include "Support.h"
 
 #include <dirent.h>
 #include <stdlib.h>
@@ -71,5 +72,32 @@ void
 RunningProcessesList::_ReadProcessInfo(process_info& info, std::string pid)
 {
 	info.pid = strtol(pid.c_str(), NULL, 10);
-	info.cmdline = ProcReader(pid.append("/cmdline").c_str()).ReadLine();
+	info.cmdline = ProcReader((pid + std::string("/cmdline")).c_str()).ReadLine();
+
+
+	// TODO: Refactor, too much duplicated code
+	ProcReader status((pid + std::string("/status")).c_str());
+
+	std::istream stream(&status);
+	std::string line;
+	try {
+		while (std::getline(stream, line) > 0) {
+			if (line.find("VmSize") != std::string::npos) {
+				size_t pos = line.find(":");
+				if (pos == std::string::npos)
+					continue;
+				std::string valueString = line.substr(pos + 2, std::string::npos);
+				info.memory = strtol(trim(valueString).c_str(), NULL, 10);
+			} else if (line.find("VmSwap") != std::string::npos) {
+				size_t pos = line.find(":");
+				if (pos == std::string::npos)
+					continue;
+				std::string valueString = line.substr(pos + 2, std::string::npos);
+				info.virtualmem = strtol(trim(valueString).c_str(), NULL, 10);
+			}
+		}
+	} catch (...) {
+
+	}
+
 }
