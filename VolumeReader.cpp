41c255be2c1191ff12cef0760fc908fdae2f0929
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-07-19 11:49:28 +0200

    Call Rewind() after populating the volume list

--- VolumeReader.cpp
+++ VolumeReader.cpp
@@ -5,11 +5,45 @@
  *      Author: stefano
  */
 
+#include "Support.h"
 #include "VolumeReader.h"
+
+#include <iostream>
+#include <sstream>
 
 VolumeReader::VolumeReader()
 {
+	popen_streambuf df("export LC_ALL=C; df", "r");
+	std::istream stream(&df);
 
+	std::string line;
+	std::getline(stream, line); // Skip the first line
+	while (std::getline(stream, line) > 0) {
+		std::istringstream iss(line);
+
+		/*try {
+			while (iss) {
+				std::string s;
+				iss >> s;
+				std::cout << s << std::endl;
+			}
+		} catch (...) {
+
+		}*/
+
+
+		volume_info info;
+		std::string dummy;
+		iss >> info.filesystem; // TODO: NO: should be ext4, etc
+		iss >> info.total;
+		iss >> dummy;
+		iss >> info.free;
+		iss >> dummy;
+		iss >> info.label;
+		fItems.push_back(info);
+	}
+
+	Rewind();
 }
 
 VolumeReader::~VolumeReader()
