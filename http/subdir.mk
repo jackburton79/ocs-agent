282d2f2ddcb857de24c831b9fa692a5f0c35dd8e
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-07-26 08:54:42 +0200

    Add makefile and other files needed to build (automatically generated by Eclipse and then edited by hand)

---
+++ http/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../http/HTTP.cpp \
+../http/HTTPDefines.cpp \
+../http/HTTPHeader.cpp \
+../http/HTTPRequestHeader.cpp \
+../http/HTTPResponseHeader.cpp 
+
+OBJS += \
+./http/HTTP.o \
+./http/HTTPDefines.o \
+./http/HTTPHeader.o \
+./http/HTTPRequestHeader.o \
+./http/HTTPResponseHeader.o 
+
+CPP_DEPS += \
+./http/HTTP.d \
+./http/HTTPDefines.d \
+./http/HTTPHeader.d \
+./http/HTTPRequestHeader.d \
+./http/HTTPResponseHeader.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+http/%.o: ../http/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DTIXML_USE_STL -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Agent.cpp \
+../Configuration.cpp \
+../IfConfigReader.cpp \
+../Inventory.cpp \
+../LoggedUsers.cpp \
+../Machine.cpp \
+../ProcReader.cpp \
+../RunningProcessesList.cpp \
+../Support.cpp \
+../VolumeReader.cpp \
+../main.cpp 
+
+OBJS += \
+./Agent.o \
+./Configuration.o \
+./IfConfigReader.o \
+./Inventory.o \
+./LoggedUsers.o \
+./Machine.o \
+./ProcReader.o \
+./RunningProcessesList.o \
+./Support.o \
+./VolumeReader.o \
+./main.o 
+
+CPP_DEPS += \
+./Agent.d \
+./Configuration.d \
+./IfConfigReader.d \
+./Inventory.d \
+./LoggedUsers.d \
+./Machine.d \
+./ProcReader.d \
+./RunningProcessesList.d \
+./Support.d \
+./VolumeReader.d \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ./%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DTIXML_USE_STL -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
