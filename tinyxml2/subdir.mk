f8647a0311902b6ca7b6f9da998fc23fd2a8b131
stefano <stefano@ced-notebook4.iacpud.local>
2014-04-30 19:43:20 +0200

    Imported tinyxml 2.1.0

---
+++ tinyxml2/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../tinyxml2/tinyxml2.cpp 
+
+OBJS += \
+./tinyxml2/tinyxml2.o 
+
+CPP_DEPS += \
+./tinyxml2/tinyxml2.d
+
+# Each subdirectory must supply rules for building sources it contributes
+tinyxml2/%.o: ../tinyxml2/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -DTIXML_USE_STL -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
