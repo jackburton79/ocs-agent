ff167c0fe765243817a85c8c17b4336eb8034ea4
JackBurton <stefano.ceccherini@gmail.com>
2014-05-11 19:51:56 +0200

    Forgot to add these

---
+++ Screens.h
@@ -0,0 +1,24 @@
+#ifndef __SCREENS_H
+#define __SCREENS_H
+
+#include "ItemsList.h"
+
+#include <string>
+
+struct screen_info {
+  std::string name;
+  std::string manufacturer;
+  std::string model;
+  std::string serial_number;  
+};
+
+class Screens : public ItemsList<screen_info> {
+public:
+  Screens();
+  
+  
+};
+
+
+#endif
+
