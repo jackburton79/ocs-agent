397dd59774805e7ebabe5c613c43f81bd2b9ae54
Jackburton79 <stefano.ceccherini@gmail.com>
2017-04-26 15:07:59 +0200

    tentative fix for crash

--- Screens.cpp
+++ Screens.cpp
@@ -15,27 +15,25 @@
 
 Screens::Screens()
 {
-    if (!CommandExists("find"))
-        return;
+    if (CommandExists("find")) {        
+		popen_streambuf buf("find /sys/devices/ -name edid", "r");
+		std::istream stream(&buf);
+		std::string line;
+		while (std::getline(stream, line)) {
+			screen_info info;
+			edid_info edidInfo;
+			info.name = line;
+			if (get_edid_info(line.c_str(), &edidInfo) == 0) {
+				info.description = edidInfo.description;
+				info.manufacturer = edidInfo.manufacturer;
+				info.model = edidInfo.model;
+				info.serial_number = edidInfo.serial_number;
+				fItems.push_back(info);
+			}
+		}
 
-    popen_streambuf buf("find /sys/devices/ -name edid", "r");
-    std::istream stream(&buf);
-
-    std::string line;
-    while (std::getline(stream, line)) {
-        screen_info info;
-        edid_info edidInfo;
-        info.name = line;
-        if (get_edid_info(line.c_str(), &edidInfo) == 0) {
-			info.description = edidInfo.description;
-            info.manufacturer = edidInfo.manufacturer;
-            info.model = edidInfo.model;
-            info.serial_number = edidInfo.serial_number;
-            fItems.push_back(info);
-        }
-    }
-
-    Rewind();
+		Rewind();
+	}
 }
 
 
