607840902167cfceb495ec4e806c76fc15e9d489
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2017-07-18 19:37:55 +0200

    Cleanup

--- Agent.cpp
+++ Agent.cpp
@@ -29,35 +29,35 @@ Agent::~Agent()
 void
 Agent::Run()
 {
+	// TODO: Either use Logger or just throw.
+	// Don't log directly to stdout/stderr.
 	Configuration* config = Configuration::Get();
+	std::string deviceID = config->DeviceID();
 	Inventory inventory;
-
-	if (!inventory.Initialize(config->DeviceID().c_str()))
+	if (!inventory.Initialize(deviceID.c_str()))
 		throw "Cannot initialize Inventory";
 
-	bool noSoftwareInventory = (config->KeyValue("nosoftware") == "true");
-	if (inventory.Build(config->DeviceID().c_str(), noSoftwareInventory)) {
-		if (config->KeyValue("stdout") == "true")
-			inventory.Print();
-		else if (!config->LocalInventory()) {
-			unsigned int secondsToWait = ::strtoul(
-				config->KeyValue("waittime").c_str(), NULL, 10);
-			std::cerr << "Waiting " << secondsToWait << " seconds..." << std::endl;
-			::sleep(secondsToWait);
-			if (inventory.Send(config->ServerURL().c_str()))
-				return;
+	bool noSoftware = (config->KeyValue("nosoftware") == "true");
+	if (!inventory.Build(deviceID.c_str(), noSoftware))
+		return;
 
-			std::cerr << "Cannot send inventory." << std::endl;
-		} else {
-			std::string fullFileName = config->OutputFileName();
-			if (fullFileName.empty()) {
-				std::cerr << "Cannot save inventory: no path or file name specified." << std::endl;
-			} else {
-				if (fullFileName[fullFileName.size() - 1] == '/')
-					fullFileName.append(config->DeviceID().c_str()).append(".xml");
-				if (!inventory.Save(config->DeviceID().c_str(), fullFileName.c_str()))
-					std::cerr << "Cannot save inventory file." << std::endl;
-			}
-		}
+	if (config->KeyValue("stdout") == "true")
+		inventory.Print();
+	else if (config->LocalInventory()) {
+		std::string fullFileName = config->OutputFileName();
+		if (!fullFileName.empty()) {
+			if (fullFileName[fullFileName.length() - 1] == '/')
+				fullFileName.append(deviceID).append(".xml");
+			inventory.Save(deviceID.c_str(), fullFileName.c_str());
+		} else
+			std::cerr << "No path/filename specified." << std::endl;
+	} else {
+		unsigned int waitSeconds = ::strtoul(
+			config->KeyValue("waittime").c_str(), NULL, 10);
+
+		std::cerr << "Waiting " << waitSeconds << " seconds..." << std::endl;
+		::sleep(waitSeconds);
+
+		inventory.Send(config->ServerURL().c_str());
 	}
 }
