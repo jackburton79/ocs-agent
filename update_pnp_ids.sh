3bdea623839eecf1faca802ba6e549c9561e8e61
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2019-07-15 19:51:25 +0000

    Remove "date added" from pnp_id array. Since we don't use it, this removes almost 100kb from the executable

--- update_pnp_ids.sh
+++ update_pnp_ids.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 wget https://uefi.org/uefi-pnp-export
-cat uefi-pnp-export | sed -r s/'<tr class="(.+)"><td>'/'{"'/  | sed -r s/'(<\/td><td>)'/\",\"/g | sed -r s/'<\/td> <\/tr>'/'\"},'/ | grep -v '<' > pnp_ids.h
-
+sed -r s/'<tr class="(.+)"><td>'/'{"'/ uefi-pnp-export | sed -r s/'<td>\d{2}\/\d{2}\/\d{4}<td>'//g | sed -r 's:<td>[[:digit:]]{2}/[[:digit:]]{2}/[[:digit:]]{4}</td>::g' | sed -r s/'(<\/td><td>)'/\",\"/g | sed -r s/'<\/td> <\/tr>'/'\"},'/ | grep -v '<' > pnp_ids.h
