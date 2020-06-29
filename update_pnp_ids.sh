088de5eef5f5d1e19d917ff6dcd511adb7887f9d
JackBurton79 <stefano.ceccherini@gmail.com>
2020-06-29 10:33:40 +0200

    Handle the case where the wget fails

--- update_pnp_ids.sh
+++ update_pnp_ids.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-wget https://uefi.org/uefi-pnp-export
-sed -r -e s/'<tr class="(.+)"><td>'/'{"'/ -e 's:<td>[[:digit:]]{2}/[[:digit:]]{2}/[[:digit:]]{4}</td>::g' -e s/'(<\/td><td>)'/\",\"/g -e s/'<\/td> <\/tr>'/'\"},'/ uefi-pnp-export | grep -v '<' > pnp_ids.h
+wget https://uefi.org/uefi-pnp-export -O pnp_ids_temp.tmp
+if [ $? -eq 0 ]; then
+	sed -r -e s/'<tr class="(.+)"><td>'/'{"'/ -e 's:<td>[[:digit:]]{2}/[[:digit:]]{2}/[[:digit:]]{4}</td>::g' -e s/'(<\/td><td>)'/\",\"/g -e s/'<\/td> <\/tr>'/'\"},'/ pnp_ids_temp.tmp | grep -v '<' > pnp_ids.h
+fi
+rm pnp_ids_temp.tmp
+	
